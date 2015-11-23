2004280f7a8a44123099c1b66ba758183e7a8515
dudusbarros <joseeduardobarros@gmail.com>
2015-11-23 20:44:21 -0200

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,44 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
+	concatCss = require('gulp-concat-css'),
 	jshint = require('gulp-jshint'),
 	minifycss = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
+	imageResize = require('gulp-image-resize'),
 	tinypng = require('gulp-tinypng');
 
-var cssFiles = 'css/*.css',
-	jsFiles = 'js/*.js',
-	imgfiles = 'img/*';
+/*
+ * To use the gulp-image-resize, it needs of some dependencies:
+ * https://www.npmjs.com/package/gulp-image-resize
+ *
+ * Or, install:
+ *
+ * Ubuntu:
+ * apt-get install imagemagick
+ * apt-get install graphicsmagick
+ *
+ * Mac:
+ * brew install imagemagick
+ * brew install graphicsmagick
+ *
+ * Windows & others:
+ * http://www.imagemagick.org/script/binary-releases.php
+ *
+ * */
+
+var cssfiles = 'css/*.css',
+	imgfiles = 'img/*.*',
+	wallpapers = 'img/wallpapers/*.*',
+	jsfiles = 'js/*.js';
+
+imgfiles = 'img/*';
 
 gulp.task('css', function() {
-	gulp.src(cssFiles)
-		.pipe(concat('villa.css'))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(minifycss())
-		.pipe(rename({
-			extname: '.min.css'
-		}))
+	gulp.src('css/*')
+		.pipe(concatCss("kabanas.css"))
 		.pipe(gulp.dest('dist/css'));
-	gulp.src([core, cssCross])
-		.pipe(concat('villa-cross.css'))
-		.pipe(gulp.dest('dist/css'))
+	gulp.src('dist/css/kabanas.css')
 		.pipe(minifycss())
 		.pipe(rename({
 			extname: '.min.css'
@@ -30,16 +47,27 @@ gulp.task('css', function() {
 });
 
 gulp.task('js', function() {
-	gulp.src(jsFiles)
+	gulp.src(jsfiles)
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
-		.pipe(uglify({
-			preserveComments: 'some'
-		}))
+		.pipe(gulp.dest('dist/js'));
+	gulp.src(['dist/js/dist.js'])
 		.pipe(rename({
 			extname: '.min.js'
 		}))
+		.pipe(uglify({
+			preserveComments: 'some'
+		}))
 		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('resizeWallpapers', function () {
+	gulp.src(wallpapers)
+		.pipe(imageResize({
+			height : 1080,
+			upscale : false
+		}))
+		.pipe(gulp.dest('img'));
 });
 
 gulp.task('tinypng', function () {
@@ -49,6 +77,8 @@ gulp.task('tinypng', function () {
 });
 
 gulp.task('default', function() {
-	var villa = ['css', 'js'];
-	gulp.watch(villa);
+	var css = ['villa', 'mowe', 'wtal'];
+	var js = ['js'];
+	gulp.watch(cssfiles, css);
+	gulp.watch(jsfiles, ['js']);
 });
